e3b90a49401659c2c7f071d5802a19dbb8188ce3
Mhmdsaqer11 <mhmdsaqer672@gmail.com>
2021-12-26 01:07:55 +0300

    changed

--- WEB_PROJESI/Controllers/HomeController.cs
+++ WEB_PROJESI/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,18 +15,22 @@ using WEB_PROJESI.Models;
 
 namespace WEB_PROJESI.Controllers
 {
-   
+    
     public class HomeController : Controller
     {
+        private readonly IHtmlLocalizer<HomeController> _localizer;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Me()
         {
+            var test = _localizer["Hi"];
+            ViewData["Hi"] = test;
             return View();
         }
 
@@ -31,9 +38,14 @@ namespace WEB_PROJESI.Controllers
         {
             return View();
         }
-        public IActionResult Contact()
+       [HttpPost]
+        public IActionResult CultureManagment(string culture,string returnUrl)
         {
-            return View();
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions {
+                    Expires = DateTimeOffset.Now.AddDays(3)
+                    });
+            return LocalRedirect(returnUrl);
         }
 
 
